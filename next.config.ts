902059ee35c5a48cfb8ff2f120c5cf7ff118af6d
trieudb3317dev <trieudb3317.dev@gamil.com>
2026-03-23 19:27:03 +0700

    Fix config image

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,17 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  /** config cloudinary image */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**.cloudinary.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
